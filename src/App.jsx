9bead2694a7e21e72dc9f55fbc7cb0f37caa8a20
SarahLakey <salbers1212@gmail.com>
2024-05-10 18:31:46 -0500

    practiced creating buttons and styling with external module and inline

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Button from './Button'
 import './App.css'
+import ButtonModule from './ButtonModule/ButtonModule'
+import ButtonInline from './ButtonInline'
+
 
 function App() {
 
   return (
     <>
      <Button/>
+     <ButtonModule/>
+     <ButtonInline/>
     </>
   )
 }
---
+++ src/ButtonModule/ButtonModule.jsx
@@ -0,0 +1,10 @@
+import styles from './ButtonModule.module.css'
+
+function ButtonModule(){
+
+    return(
+        <button className={styles.buttonModule}>Click me!</button>
+    );
+}
+
+export default ButtonModule
