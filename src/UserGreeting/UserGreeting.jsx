d3f6203842508aadb9f0e4a470432a4b696ec15b
SarahLakey <salbers1212@gmail.com>
2024-05-12 12:06:32 -0500

    updated login props and css styling

--- src/UserGreeting/UserGreeting.jsx
+++ src/UserGreeting/UserGreeting.jsx
@@ -1,13 +1,27 @@
 import styles from './UserGreeting.module.css'
+import PropTypes from 'prop-types';
 
 function UserGreeting(props){
 
-    return(props.isLoggedIn ? 
-        <h1 className={styles.welcomeMessage}>Welcome, {props.username}!</h1> :
-        <h1 className={styles.greeting}>Please log in to continue.</h1>
-    
+    const welcomeMessage = <h1 className={styles.welcomeMessage}>
+                            Welcome, {props.username}!</h1> 
+
+    const loginPrompt = <h1 className={styles.loginPrompt}>
+                            Please log in to continue.</h1>
+
+
+    return(props.isLoggedIn ?  welcomeMessage : loginPrompt
         );
     }
 
+    UserGreeting.prototype = {
+        isLoggedIn: PropTypes.bool,
+        username: PropTypes.string,
+    }
+
+    // UserGreeting.defaultProps={
+    //     isLoggedIn: false,
+    //     username: "Guest",
+    // }
 
 export default UserGreeting
