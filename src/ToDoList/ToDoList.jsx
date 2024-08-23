f999860182aa7b90ad324673fc60acbddfbfe92c
SarahLakey <salbers1212@gmail.com>
2024-08-23 13:58:14 -0500

    working on creating a to do list using react components

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -2,11 +2,51 @@ import React, { useState } from 'react'
 
 function ToDoList(){
 
-    return(
-        <>
-            <h1> Hello! </h1>
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
 
-        </>
+    //to see the text within the text box
+    function handleInputChange(event){
+        setNewTask(event.target.value);
+    }
+
+    function addTask(){
+
+    }
+
+    function deleteTask(index){
+
+    }
+
+    function moveTaskUp(index){
+
+    }
+
+    function moveTaskDown(index){
+
+    }
+
+    return(
+        <div className='to-do-list'>
+            <h1> To Do List: </h1>
+            <div>
+                <input
+                    type="text"
+                    placeholder='Enter a task...'
+                    value={newTask}
+                    onChange={handleInputChange}
+                    />
+                <button
+                className='add-button'
+                onClick={addTask}>
+                    Add
+                </button>
+            </div>
+    
+            <h6>*Pending*</h6>
+
+        </div>
+        
         )
     }
 
